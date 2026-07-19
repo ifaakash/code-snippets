2c0454c5bdf0ae9e5fd80d2aa450f09c77b4c901
ifaakash <aakashc.appregis@gmail.com>
2026-07-19 21:19:13 +0530

    feat(ch3): add program to list directory contents using system()
    Implement a simple C program that uses the `system()` function to execute the `ls`
    command and display directory contents.

---
+++ ch3/system.c
@@ -0,0 +1,20 @@
+/*
+
+Input:
+- NA
+
+Output:
+- List the content of the directory
+
+ALOGRITHM:
+
+Use the system() function from the stdio library
+
+*/
+
+#include <stdlib.h>
+
+int main(){
+  system("ls");
+  return 0;
+}
