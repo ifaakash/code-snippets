73542422cd76f637431277f368d083e4102f9490
ifaakash <aakashc.appregis@gmail.com>
2026-07-20 17:19:23 +0530

    feat(ch3): add integer to character conversion implementation

--- ch3/convert-integer-to-char.c
+++ ch3/convert-integer-to-char.c
@@ -7,5 +7,16 @@ Output:
 - ASCII character corresponding to number
 
 ALGORITHM:
-
+Enter input as integer
+Do type conversion of integer to char
+print the character
 */
+
+
+#include<stdio.h>
+void main(){
+  int iinput;
+  printf("Enter any character:\n");
+  scanf("%d", &iinput);
+  printf("The character corresponding to the integer value %d is %c\n", iinput, (char)iinput);
+}
