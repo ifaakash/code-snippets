97c7aa033d21142b90c6257275c9f8ce151318ac
ifaakash <aakashc.appregis@gmail.com>
2026-07-21 15:10:16 +0530

    feat(looping-structures): add count-to-100 examples with inc5 and dec10
    Add two C programs:
    - Count from 0 to 100 with increment of 5 using a while loop
    - Count from 100 down to 1 with decrement of 10 using a while loop

---
+++ ch4-looping-structures/count-to-1-with-dec-10.c
@@ -0,0 +1,23 @@
+/*
+
+Input:
+- NA
+
+Output:
+- Seq from 1 to 100 with increment 5
+
+ALGORITHM:
+Start a while loop from 1 to <=100
+increment of +5 on each iteration
+
+*/
+
+#include<stdio.h>
+int main(){
+  int i=100;
+  while(i>=1){
+    printf("%d\n", i);
+    i -=10;
+  }
+  return 0;
+}
