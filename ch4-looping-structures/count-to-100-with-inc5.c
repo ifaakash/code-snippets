f00851bedc665e83257515b84f2a6fb516dcb307
ifaakash <aakashc.appregis@gmail.com>
2026-07-21 15:19:00 +0530

    feat(ch4-looping-structures): add alternative while loop examples for multiples of 5

--- ch4-looping-structures/count-to-100-with-inc5.c
+++ ch4-looping-structures/count-to-100-with-inc5.c
@@ -19,5 +19,18 @@ int main(){
     printf("%d\n", i);
     i +=5;
   }
+  printf("----------------- PRINTING MULTIPLE OF 5 -----------------\n");
+  i=1;
+  while(i<=20){
+    printf("%d\n", i*5);
+    i++;
+  }
+  
+  printf("----------------- PRINTING UPTO 100 with INCREMENT OF 5 -----------------\n");
+  i=1;
+  while(i<=100){
+    printf("%d\n", i);
+    i +=5;
+  }
   return 0;
 }
